54749dd9d8689c87d36bb20b2452778ffaeabdfe
equiman <equiman@users.noreply.github.com>
2017-12-21 14:41:19 -0500

    Add System skip test for control

---
+++ ToolBox.Tests/System/MachineTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using ToolBox;
+using ToolBox.System.Command;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ToolBox.System.Tests
+{
+    public class MachineTests
+    {
+        public MachineTests()
+        {
+            //Arrange
+        }
+
+        [Fact(Skip="It's System Functionality")]
+        public void GetUser_WhenCalls_NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(Skip="It's System Functionality")]
+        public void GetDomain_WhenCalls_NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
