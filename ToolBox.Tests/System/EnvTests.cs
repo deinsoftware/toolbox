002bcae71163dbccbc2665156e7ab042e4f4db1c
equiman <equiman@users.noreply.github.com>
2017-12-21 15:08:02 -0500

    Add env system skip tests

---
+++ ToolBox.Tests/System/EnvTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using ToolBox;
+using ToolBox.System.Command;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ToolBox.System.Tests
+{
+    public class EnvTests
+    {
+        public EnvTests()
+        {
+            //Arrange
+        }
+
+        [Fact(Skip="It's System Functionality")]
+        public void GetValue_WhenCalls_NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(Skip="It's System Functionality")]
+        public void SetValue_WhenCalls_NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(Skip="It's System Functionality")]
+        public void IsNullOrEmpty_WhenCalls_NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
