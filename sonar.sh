efd62faad1d6cd853591c9917e6a3c21da32addf
equiman <equiman@users.noreply.github.com>
2018-01-12 15:55:15 -0500

    Secure SonarCloud token

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.1.2" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.1.2" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test ToolBox.Tests/ToolBox.Tests.csproj
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6"
+mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login="${token}"
