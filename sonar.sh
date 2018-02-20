5687fbcb59ca7def2a3da43f3cd1925e065d90c9
equiman <equiman@users.noreply.github.com>
2018-02-19 21:30:53 -0500

    1.1.3 - Fixed: NuGet package files, remove source files and pack under lib folder.

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.1.2" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.1.3" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test ToolBox.Tests/ToolBox.Tests.csproj
