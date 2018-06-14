48ac98b74db20304d381c568ddf0e8559e6b984c
equiman <equiman@users.noreply.github.com>
2018-06-14 10:30:12 -0500

    Sonar scanner update for mac

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-mono ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.2.0" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll begin /k:"dein:toolbox" /n:"ToolBox" /v:"1.2.0" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test ToolBox.Tests/ToolBox.Tests.csproj
-mono ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll end /d:sonar.login="${token}"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll end /d:sonar.login="${token}"
